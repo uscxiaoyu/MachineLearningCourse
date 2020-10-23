e971a17945bc92fd69e466fab860bd6ccc596e27
mainserver <jxndtbxiaoyu@163.com>
2020-10-23 23:04:42 +0800

    server

--- py/optimize_methods_tensor.py
+++ py/optimize_methods_tensor.py
@@ -4,92 +4,85 @@ import torch
 import numpy as np
 
 
-def grad_desc(lossfunc, x0, x1, max_iters=200, learn_rate=0.05):
+def grad_desc(lossfunc, w, x_dict, learn_rate=0.05, max_iters=1000):
     """
-    f: 待优化目标函数, grad_f: f的梯度, x0: 参数初值, x1:固定参数值, learn_rate: 学习率
+    f: 待优化目标函数, grad_f: f的梯度, w: 参数初值, x_dict:固定参数值, learn_rate: 学习率
     """
-    trace_x = x0.clone().data.reshape(1, -1)
-    x = x0
+    trace_w = w.clone().data.reshape(1, -1)
     i = 0
     for i in range(max_iters):
-        l = lossfunc(x, x1)
+        l = lossfunc(w=w, **x_dict)
         l.backward()
-        x.data.sub_(learn_rate * x.grad.data)
+        w.data.sub_(learn_rate * w.grad.data)
         with torch.no_grad():
-            trace_x = torch.cat([trace_x, x.detach().data.reshape(1, -1)], 0)
-            if i % 10 == 0:
-                loss = lossfunc(x, x1).data.numpy()
-                print(f"迭代次数: {i}, 损失函数值: {loss:.4f}")
-                
-            if torch.sum(torch.abs(trace_x[-1] - trace_x[-2])) < 1e-3:  # 停止条件
+            trace_w = torch.cat([trace_w, w.detach().data.reshape(1, -1)], 0)
+            if (i + 1) % 10 == 0:
+                loss = lossfunc(w=w, **x_dict).data.numpy()
+                print(f"迭代次数: {i+1}, 损失函数值: {loss:.4f}")
+
+            if torch.sum(torch.abs(trace_w[-1] - trace_w[-2])) < 1e-3:  # 停止条件
                 break
 
-        x.grad.zero_()
+        w.grad.zero_()
 
-    print(f"共迭代{i}次, 损失函数值: {lossfunc(x, x1).data.numpy():.4f}, 最优参数值: {x.tolist()}")
-    return trace_x
+    print(f"共迭代{i}次, 损失函数值: {lossfunc(w=w, **x_dict).data.numpy():.4f}, 最优参数值: {w.tolist()}")
+    return trace_w
 
 
-def grad_desc_with_momentum(f, grad_f, x0, beta=0.5, learn_rate=0.05):
-    trace_x = np.array([x0])
-    x = x0
+def grad_desc_with_momentum(lossfunc, w, x_dict, beta=0.5, learn_rate=0.05, max_iter=1000):
+    trace_w = w.clone().data.reshape(1, -1)
     m_0 = 0
     i = 0
     while True:
-        grad = grad_f(x)
-        m_1 = beta * m_0 + (1 - beta) * grad
-        x = x - learn_rate * m_1
-        trace_x = np.concatenate([trace_x, x.reshape(1, -1)])
-        if i % 5 == 0:
-            print(f"迭代次数: {i}, 目标函数值f: {f(x):.6f}")
+        l = lossfunc(w=w, **x_dict)
+        l.backward()
+        m_1 = beta * m_0 + (1 - beta) * w.grad.data
+        w.data.sub_(learn_rate * m_1)
+        with torch.no_grad():
+            trace_w = torch.cat([trace_w, w.detach().data.reshape(1, -1)], 0)
+            if (i + 1) % 10 == 0:
+                loss = lossfunc(w=w, **x_dict).data.numpy()
+                print(f"迭代次数: {i+1}, 损失函数值: {loss:.4f}")
 
-        if np.sum(np.abs(trace_x[-1] - trace_x[-2])) < 1e-3:  # 停止条件
-            break
+            if torch.sum(torch.abs(trace_w[-1] - trace_w[-2])) < 1e-3 or i > max_iter:  # 停止条件
+                break
 
+        w.grad.zero_()
         m_0 = m_1
         i += 1
 
-    print(f"共迭代{len(trace_x)}次, 目标函数: {f(x)}, 最优参数值: {x.tolist()}")
-    return trace_x
+    print(f"共迭代{i}次, 损失函数值: {lossfunc(w=w, **x_dict).data.numpy():.4f}, 最优参数值: {w.tolist()}")
+    return trace_w
 
 
-def adaptive_momentum(f, grad_f, x0, beta1=0.5, beta2=0.5, learn_rate=0.05):
-    trace_x = np.array([x0])
-    x = x0
+def adaptive_momentum(lossfunc, w, x_dict, beta1=0.5, beta2=0.5, learn_rate=0.05, max_iter=1000):
+    trace_w = w.clone().data.reshape(1, -1)
     m_0, v_0 = 0, 0
     i = 0
     while True:
-        grad = grad_f(x)
-        m_1 = beta1 * m_0 + (1 - beta1) * grad
-        v_1 = beta2 * v_0 + (1 - beta2) * grad ** 2
-        x = x - learn_rate * m_1 / np.sqrt(v_1)
-        trace_x = np.concatenate([trace_x, x.reshape(1, -1)])
-        if i % 5 == 0:
-            print(f"迭代次数: {i}, 目标函数值f: {f(x):.6f}")
+        l = lossfunc(w=w, **x_dict)
+        l.backward()
+        m_1 = beta1 * m_0 + (1 - beta1) * w.grad.data
+        v_1 = beta2 * v_0 + (1 - beta2) * w.grad.data ** 2
+        w.data.sub_(learn_rate * m_1 / torch.sqrt(v_1))
+        with torch.no_grad():
+            trace_w = torch.cat([trace_w, w.detach().data.reshape(1, -1)], 0)
+            if (i + 1) % 10 == 0:
+                loss = lossfunc(w=w, **x_dict).data.numpy()
+                print(f"迭代次数: {i+1}, 损失函数值: {loss:.4f}")
 
-        if np.sum(np.abs(trace_x[-1] - trace_x[-2])) < 1e-3:  # 停止条件
-            break
+            if torch.sum(torch.abs(trace_w[-1] - trace_w[-2])) < 1e-3 or i > max_iter:  # 停止条件
+                break
 
+        w.grad.zero_()
         m_0, v_0 = m_1, v_1
         i += 1
 
-    print(f"共迭代{len(trace_x)}次, 目标函数: {f(x)}, 最优参数值: {x.tolist()}")
-    return trace_x
+    print(f"共迭代{i}次, 损失函数值: {lossfunc(w=w, **x_dict).data.numpy():.4f}, 最优参数值: {w.tolist()}")
+    return trace_w
 
 
 if __name__ == "__main__":
-
-    def f(x):
-        """
-        函数: f(x0, x1) = x0**2 + x1**2
-        """
-        return x[0] ** 2 + 2 * x[1] ** 2  # objective
-
-    def grad_f(x):
-        """
-        f(x)的梯度
-        """
-        return np.array([2 * x[0], 4 * x[1]])  # gradient
 
     # res = grad_desc(f, grad_f, x0=np.array([3, 3]), learn_rate=0.2)
     res = grad_desc_with_momentum(f, grad_f, x0=np.array([3, 3]), beta=0.5, learn_rate=0.2)
